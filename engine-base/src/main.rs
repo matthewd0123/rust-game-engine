aef5aee45597db08b75e1cf73aca42a7ffc8ec88
Matthew D'Alonzo <matthewdalonzo@gmail.com>
2025-02-10 21:29:38 -0500

    Base Commit

---
+++ engine-base/src/main.rs
@@ -0,0 +1,9 @@
+use ash::version::EntryV1_0;
+use ash::version::InstanceV1_0;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let entry = ash::Entry::new()?;
+    let instance = unsafe { entry.create_instance(&Default::default(), None)? };
+    unsafe { instance.destroy_instance(None) };
+    Ok(())
+}
